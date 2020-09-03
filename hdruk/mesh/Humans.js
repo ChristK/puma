dd2c2a2a4f33c394652dff710cc50b2d113fa99b
Azure pipeline <you@example.com>
2020-09-03 01:56:57 +0000

    Automatically updating web files for hdruk

--- hdruk/mesh/Humans.js
+++ hdruk/mesh/Humans.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2018',21],['2019',46],]);var citations =([['Year', 'Number of Citations'],['2018',730],['2019',569],]);
+var papers =([['Year', 'Number of papers'],['2018',21],['2019',46],]);var citations =([['Year', 'Number of Citations'],['2018',730],['2019',570],]);
