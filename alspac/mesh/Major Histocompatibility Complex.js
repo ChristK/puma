6701ebf02abfd132844e7d38bad45fe288fe3c35
Azure pipeline <you@example.com>
2020-09-03 01:53:25 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Major Histocompatibility Complex.js
+++ alspac/mesh/Major Histocompatibility Complex.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2010',1],]);var citations =([['Year', 'Number of Citations'],['2010',431],]);
+var papers =([['Year', 'Number of papers'],['2010',1],]);var citations =([['Year', 'Number of Citations'],['2010',432],]);
