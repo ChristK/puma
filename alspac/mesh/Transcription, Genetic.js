1479d92eac7a584d5820a3e04aa1150991956cae
Azure pipeline <you@example.com>
2020-09-05 01:54:39 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Transcription, Genetic.js
+++ alspac/mesh/Transcription, Genetic.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2011',1],['2012',1],]);var citations =([['Year', 'Number of Citations'],['2011',265],['2012',460],]);
+var papers =([['Year', 'Number of papers'],['2011',1],['2012',1],]);var citations =([['Year', 'Number of Citations'],['2011',265],['2012',461],]);
