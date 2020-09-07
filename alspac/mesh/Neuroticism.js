63af1466e722720ab34380b1ce490aa746ed1cea
Azure pipeline <you@example.com>
2020-09-07 01:52:02 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Neuroticism.js
+++ alspac/mesh/Neuroticism.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',1],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2014',62],['2015',136],]);
+var papers =([['Year', 'Number of papers'],['2014',1],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2014',62],['2015',137],]);
