d22a34ea197351c40606ba5401ae0a0f77fc18bf
Azure pipeline <you@example.com>
2020-09-23 01:50:25 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Depressive Disorder, Major.js
+++ alspac/mesh/Depressive Disorder, Major.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2013',280],['2014',31],['2015',199],]);
+var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2013',280],['2014',31],['2015',200],]);
--- alspac/mesh/Personality.js
+++ alspac/mesh/Personality.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',2],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2014',82],['2015',296],]);
+var papers =([['Year', 'Number of papers'],['2014',2],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2014',82],['2015',297],]);
