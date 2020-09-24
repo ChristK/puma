d9de04d2b185bd0f56ef2df15553ef6139dbfdbc
Azure pipeline <you@example.com>
2020-09-24 01:54:11 +0000

    Automatically updating web files for hdruk

--- hdruk/mesh/Female.js
+++ hdruk/mesh/Female.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2018',14],['2019',31],]);var citations =([['Year', 'Number of Citations'],['2018',705],['2019',452],]);
+var papers =([['Year', 'Number of papers'],['2018',14],['2019',31],]);var citations =([['Year', 'Number of Citations'],['2018',705],['2019',453],]);
