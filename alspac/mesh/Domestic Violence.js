e9d09dfda8dd03dabb2f2beeab260fb0c237064b
Azure pipeline <you@example.com>
2020-09-25 01:52:02 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Domestic Violence.js
+++ alspac/mesh/Domestic Violence.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2011',1],['2012',0],['2013',1],]);var citations =([['Year', 'Number of Citations'],['2011',75],['2012',0],['2013',104],]);
+var papers =([['Year', 'Number of papers'],['2011',1],['2012',0],['2013',1],]);var citations =([['Year', 'Number of Citations'],['2011',75],['2012',0],['2013',105],]);
