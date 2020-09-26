ec4419c6b1409ab3b306e51047ed2240423549bd
Azure pipeline <you@example.com>
2020-09-26 01:59:40 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Delivery of Health Care.js
+++ clahrc/mesh/Delivery of Health Care.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',2],]);var citations =([['Year', 'Number of Citations'],['2016',5],['2017',6],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',2],]);var citations =([['Year', 'Number of Citations'],['2016',5],['2017',7],]);
--- clahrc/mesh/Social Support.js
+++ clahrc/mesh/Social Support.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2015',1],['2016',0],['2017',2],]);var citations =([['Year', 'Number of Citations'],['2015',0],['2016',0],['2017',9],]);
+var papers =([['Year', 'Number of papers'],['2015',1],['2016',0],['2017',2],]);var citations =([['Year', 'Number of Citations'],['2015',1],['2016',0],['2017',10],]);
