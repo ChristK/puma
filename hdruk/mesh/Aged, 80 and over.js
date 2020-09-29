9f7eb9efb6bcc17a237246f96f7b1e6e0dfc8180
Azure pipeline <you@example.com>
2020-09-29 01:54:24 +0000

    Automatically updating web files for hdruk

--- hdruk/mesh/Aged, 80 and over.js
+++ hdruk/mesh/Aged, 80 and over.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2018',8],['2019',9],]);var citations =([['Year', 'Number of Citations'],['2018',700],['2019',344],]);
+var papers =([['Year', 'Number of papers'],['2018',8],['2019',9],]);var citations =([['Year', 'Number of Citations'],['2018',701],['2019',344],]);
