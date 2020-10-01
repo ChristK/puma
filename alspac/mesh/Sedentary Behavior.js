8a3ca2721a3f151ed79ea164ebdffaeebbd09fac
Azure pipeline <you@example.com>
2020-10-01 01:51:20 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Sedentary Behavior.js
+++ alspac/mesh/Sedentary Behavior.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',3],['2013',0],['2014',1],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2012',821],['2013',0],['2014',18],['2015',377],]);
+var papers =([['Year', 'Number of papers'],['2012',3],['2013',0],['2014',1],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2012',822],['2013',0],['2014',18],['2015',377],]);
