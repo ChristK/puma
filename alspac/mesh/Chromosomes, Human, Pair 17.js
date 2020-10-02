00719c66adcd292a6d244aee19f7308261a5dfcf
Azure pipeline <you@example.com>
2020-10-02 01:49:34 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Chromosomes, Human, Pair 17.js
+++ alspac/mesh/Chromosomes, Human, Pair 17.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],]);var citations =([['Year', 'Number of Citations'],['2013',40],['2014',25],]);
+var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],]);var citations =([['Year', 'Number of Citations'],['2013',41],['2014',25],]);
--- alspac/mesh/Pediatric Obesity.js
+++ alspac/mesh/Pediatric Obesity.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',3],['2014',3],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2013',143],['2014',73],['2015',154],]);
+var papers =([['Year', 'Number of papers'],['2013',3],['2014',3],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2013',144],['2014',73],['2015',154],]);
