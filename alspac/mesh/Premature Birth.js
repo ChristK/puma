69f77171d9c698aed223f11497edd8413c185eb4
Azure pipeline <you@example.com>
2020-10-07 01:50:19 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Premature Birth.js
+++ alspac/mesh/Premature Birth.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',3],['2013',2],['2014',1],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2012',427],['2013',122],['2014',173],['2015',109],]);
+var papers =([['Year', 'Number of papers'],['2012',3],['2013',2],['2014',1],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2012',427],['2013',122],['2014',173],['2015',110],]);
