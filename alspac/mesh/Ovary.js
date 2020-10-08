ff6839718d48a0795dab6a5a7ed073403746a39e
Azure pipeline <you@example.com>
2020-10-08 01:50:20 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Ovary.js
+++ alspac/mesh/Ovary.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2013',22],['2014',275],['2015',130],]);
+var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2013',22],['2014',275],['2015',131],]);
