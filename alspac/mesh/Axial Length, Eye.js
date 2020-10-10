aafd953402f29c08e86526d518010693abfef345
Azure pipeline <you@example.com>
2020-10-10 01:48:52 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Axial Length, Eye.js
+++ alspac/mesh/Axial Length, Eye.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',2],['2014',0],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2013',101],['2014',0],['2015',4],]);
+var papers =([['Year', 'Number of papers'],['2013',2],['2014',0],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2013',102],['2014',0],['2015',4],]);
--- alspac/mesh/Pregnancy Complications, Cardiovascular.js
+++ alspac/mesh/Pregnancy Complications, Cardiovascular.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',2],]);var citations =([['Year', 'Number of Citations'],['2012',295],]);
+var papers =([['Year', 'Number of papers'],['2012',2],]);var citations =([['Year', 'Number of Citations'],['2012',296],]);
