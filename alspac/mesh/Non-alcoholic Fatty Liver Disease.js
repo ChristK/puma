c2b70db883600d1963f7603b143fe1f8c42c4767
Azure pipeline <you@example.com>
2020-10-19 01:51:19 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Non-alcoholic Fatty Liver Disease.js
+++ alspac/mesh/Non-alcoholic Fatty Liver Disease.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',2],['2015',2],]);var citations =([['Year', 'Number of Citations'],['2014',60],['2015',16],]);
+var papers =([['Year', 'Number of papers'],['2014',2],['2015',2],]);var citations =([['Year', 'Number of Citations'],['2014',61],['2015',17],]);
