28e50c23e8e549a32dc95aaefcf48ebddcd79b2f
Azure pipeline <you@example.com>
2020-10-31 01:47:46 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Metabolic Networks and Pathways.js
+++ alspac/mesh/Metabolic Networks and Pathways.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',1],['2013',1],]);var citations =([['Year', 'Number of Citations'],['2012',275],['2013',165],]);
+var papers =([['Year', 'Number of papers'],['2012',1],['2013',1],]);var citations =([['Year', 'Number of Citations'],['2012',276],['2013',166],]);
