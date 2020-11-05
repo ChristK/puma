64b602046f602afc5beba193073259ddca61091b
Azure pipeline <you@example.com>
2020-11-05 02:02:01 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/United Kingdom.js
+++ clahrc/mesh/United Kingdom.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',4],['2017',5],['2018',4],['2019',2],]);var citations =([['Year', 'Number of Citations'],['2016',10],['2017',26],['2018',30],['2019',4],]);
+var papers =([['Year', 'Number of papers'],['2016',4],['2017',5],['2018',4],['2019',2],]);var citations =([['Year', 'Number of Citations'],['2016',10],['2017',26],['2018',31],['2019',4],]);
