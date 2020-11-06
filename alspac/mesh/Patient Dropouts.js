f7dcd5848cd04817a53b84fd3dafc4cb670c7aa9
Azure pipeline <you@example.com>
2020-11-06 01:52:36 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Patient Dropouts.js
+++ alspac/mesh/Patient Dropouts.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2009',1],]);var citations =([['Year', 'Number of Citations'],['2009',344],]);
+var papers =([['Year', 'Number of papers'],['2009',1],]);var citations =([['Year', 'Number of Citations'],['2009',347],]);
