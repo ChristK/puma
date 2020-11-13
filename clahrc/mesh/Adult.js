0eb4a192fe9523be54901bb574ac14076089de63
Azure pipeline <you@example.com>
2020-11-13 02:06:34 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Adult.js
+++ clahrc/mesh/Adult.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',7],['2017',8],['2018',11],['2019',11],]);var citations =([['Year', 'Number of Citations'],['2016',85],['2017',65],['2018',87],['2019',24],]);
+var papers =([['Year', 'Number of papers'],['2016',7],['2017',8],['2018',11],['2019',11],]);var citations =([['Year', 'Number of Citations'],['2016',85],['2017',66],['2018',87],['2019',24],]);
--- clahrc/mesh/Young Adult.js
+++ clahrc/mesh/Young Adult.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',5],['2018',6],['2019',6],]);var citations =([['Year', 'Number of Citations'],['2016',5],['2017',88],['2018',67],['2019',16],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',5],['2018',6],['2019',6],]);var citations =([['Year', 'Number of Citations'],['2016',5],['2017',89],['2018',67],['2019',16],]);
