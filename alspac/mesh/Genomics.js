e3e14a814e17d5e8550a7b5b35d5660a62b7ace8
Azure pipeline <you@example.com>
2020-11-19 01:47:23 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Genomics.js
+++ alspac/mesh/Genomics.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2013',154],['2014',74],['2015',747],]);
+var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2013',154],['2014',74],['2015',748],]);
--- alspac/mesh/Suicide, Attempted.js
+++ alspac/mesh/Suicide, Attempted.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',1],['2013',0],['2014',3],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2012',67],['2013',0],['2014',94],['2015',85],]);
+var papers =([['Year', 'Number of papers'],['2012',1],['2013',0],['2014',3],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2012',67],['2013',0],['2014',94],['2015',86],]);
