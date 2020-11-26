8f39563cbdcf79e69d393eb7a37f992b2a44b9b0
Azure pipeline <you@example.com>
2020-11-26 02:01:37 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Risk Factors.js
+++ clahrc/mesh/Risk Factors.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2017',3],['2018',2],['2019',1],]);var citations =([['Year', 'Number of Citations'],['2017',21],['2018',13],['2019',2],]);
+var papers =([['Year', 'Number of papers'],['2017',3],['2018',2],['2019',1],]);var citations =([['Year', 'Number of Citations'],['2017',22],['2018',13],['2019',2],]);
