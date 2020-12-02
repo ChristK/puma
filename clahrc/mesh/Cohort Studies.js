556bde47317fc2b9babb3450655ab6d38b4842b9
Azure pipeline <you@example.com>
2020-12-02 02:01:22 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Cohort Studies.js
+++ clahrc/mesh/Cohort Studies.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',2],['2018',0],['2019',2],]);var citations =([['Year', 'Number of Citations'],['2016',5],['2017',10],['2018',0],['2019',8],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',2],['2018',0],['2019',2],]);var citations =([['Year', 'Number of Citations'],['2016',5],['2017',10],['2018',0],['2019',9],]);
