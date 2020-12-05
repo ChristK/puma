75c3b4dc285a832100840cc0e962066dc7bd8ceb
Azure pipeline <you@example.com>
2020-12-05 01:49:52 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Irritable Mood.js
+++ alspac/mesh/Irritable Mood.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',1],['2013',1],['2014',1],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2012',38],['2013',69],['2014',50],['2015',24],]);
+var papers =([['Year', 'Number of papers'],['2012',1],['2013',1],['2014',1],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2012',38],['2013',69],['2014',51],['2015',24],]);
--- alspac/mesh/Suicidal Ideation.js
+++ alspac/mesh/Suicidal Ideation.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',2],['2013',0],['2014',5],['2015',5],]);var citations =([['Year', 'Number of Citations'],['2012',200],['2013',0],['2014',214],['2015',115],]);
+var papers =([['Year', 'Number of papers'],['2012',2],['2013',0],['2014',5],['2015',5],]);var citations =([['Year', 'Number of Citations'],['2012',200],['2013',0],['2014',215],['2015',115],]);
