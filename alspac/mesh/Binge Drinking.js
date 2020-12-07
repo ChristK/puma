98c301bbd80197b5b83f5d318f64fa5053b56104
Azure pipeline <you@example.com>
2020-12-07 01:51:22 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Binge Drinking.js
+++ alspac/mesh/Binge Drinking.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],['2015',2],]);var citations =([['Year', 'Number of Citations'],['2013',31],['2014',27],['2015',78],]);
+var papers =([['Year', 'Number of papers'],['2013',1],['2014',1],['2015',2],]);var citations =([['Year', 'Number of Citations'],['2013',31],['2014',27],['2015',80],]);
