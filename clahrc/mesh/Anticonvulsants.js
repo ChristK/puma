791a137a5ba204cbd2ae79771aa18dcef95881b0
Azure pipeline <you@example.com>
2020-12-10 02:00:01 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Anticonvulsants.js
+++ clahrc/mesh/Anticonvulsants.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',6],['2017',5],['2018',7],['2019',3],]);var citations =([['Year', 'Number of Citations'],['2016',167],['2017',37],['2018',90],['2019',4],]);
+var papers =([['Year', 'Number of papers'],['2016',6],['2017',5],['2018',7],['2019',3],]);var citations =([['Year', 'Number of Citations'],['2016',167],['2017',38],['2018',90],['2019',4],]);
