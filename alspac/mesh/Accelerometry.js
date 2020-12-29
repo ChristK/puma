9410233b1232754c268b2a317463b8d7c37aaa1f
Azure pipeline <you@example.com>
2020-12-29 01:53:24 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Accelerometry.js
+++ alspac/mesh/Accelerometry.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',4],['2013',0],['2014',1],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2012',125],['2013',0],['2014',23],['2015',346],]);
+var papers =([['Year', 'Number of papers'],['2012',4],['2013',0],['2014',1],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2012',125],['2013',0],['2014',23],['2015',347],]);
