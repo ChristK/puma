bd4850005497c9c451021e93fe9bff5faad4c09d
Azure pipeline <you@example.com>
2021-01-08 01:45:07 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Survivors.js
+++ alspac/mesh/Survivors.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2004',1],]);var citations =([['Year', 'Number of Citations'],['2004',209],]);
+var papers =([['Year', 'Number of papers'],['2004',1],]);var citations =([['Year', 'Number of Citations'],['2004',210],]);
