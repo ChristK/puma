913d01bd1049b066bb7ea584d467dd1308a81f29
Azure pipeline <you@example.com>
2021-01-09 01:44:09 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Lip.js
+++ alspac/mesh/Lip.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2013',2],['2014',1],]);var citations =([['Year', 'Number of Citations'],['2013',19],['2014',10],]);
+var papers =([['Year', 'Number of papers'],['2013',2],['2014',1],]);var citations =([['Year', 'Number of Citations'],['2013',20],['2014',10],]);
