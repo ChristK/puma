cb5441d9bdd978d2593133fdbaed0a3e1b37646a
Azure pipeline <you@example.com>
2021-01-14 01:53:34 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Carbamazepine.js
+++ clahrc/mesh/Carbamazepine.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',2],['2017',1],['2018',3],]);var citations =([['Year', 'Number of Citations'],['2016',6],['2017',0],['2018',77],]);
+var papers =([['Year', 'Number of papers'],['2016',2],['2017',1],['2018',3],]);var citations =([['Year', 'Number of Citations'],['2016',6],['2017',0],['2018',78],]);
