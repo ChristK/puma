c5541cd3460edc1a7098966df4bb16dd91aa809a
Azure pipeline <you@example.com>
2021-01-17 01:59:51 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Female.js
+++ clahrc/mesh/Female.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',8],['2017',13],['2018',16],['2019',15],]);var citations =([['Year', 'Number of Citations'],['2016',194],['2017',144],['2018',117],['2019',41],]);
+var papers =([['Year', 'Number of papers'],['2016',8],['2017',13],['2018',16],['2019',15],]);var citations =([['Year', 'Number of Citations'],['2016',194],['2017',145],['2018',117],['2019',41],]);
