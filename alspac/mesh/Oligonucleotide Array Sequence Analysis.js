70fb65b13a57cbf4e6c62eb0957e15cb41511a3b
Azure pipeline <you@example.com>
2021-02-01 01:49:09 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Oligonucleotide Array Sequence Analysis.js
+++ alspac/mesh/Oligonucleotide Array Sequence Analysis.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2007',1],]);var citations =([['Year', 'Number of Citations'],['2007',1670],]);
+var papers =([['Year', 'Number of papers'],['2007',1],]);var citations =([['Year', 'Number of Citations'],['2007',1671],]);
--- alspac/mesh/Premature Birth.js
+++ alspac/mesh/Premature Birth.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',3],['2013',2],['2014',1],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2012',447],['2013',126],['2014',181],['2015',117],]);
+var papers =([['Year', 'Number of papers'],['2012',3],['2013',2],['2014',1],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2012',448],['2013',126],['2014',181],['2015',117],]);
