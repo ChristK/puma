3ee61a48c198d19f67f414440a5802083f881c6b
Azure pipeline <you@example.com>
2021-02-06 01:46:06 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Exome.js
+++ alspac/mesh/Exome.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',1],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2014',56],['2015',718],]);
+var papers =([['Year', 'Number of papers'],['2014',1],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2014',57],['2015',718],]);
