3bec27a633c045bcbed29711ce7ca446fce4a136
Azure pipeline <you@example.com>
2021-02-10 01:47:17 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Bullying.js
+++ alspac/mesh/Bullying.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',3],['2013',4],['2014',4],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2012',255],['2013',349],['2014',249],['2015',368],]);
+var papers =([['Year', 'Number of papers'],['2012',3],['2013',4],['2014',4],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2012',255],['2013',349],['2014',250],['2015',368],]);
