1da0e422c80eaf16238520f3727400ae0fc24ff2
Azure pipeline <you@example.com>
2021-02-12 02:06:35 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Epilepsies, Partial.js
+++ clahrc/mesh/Epilepsies, Partial.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',3],['2017',1],['2018',3],]);var citations =([['Year', 'Number of Citations'],['2016',26],['2017',0],['2018',20],]);
+var papers =([['Year', 'Number of papers'],['2016',3],['2017',1],['2018',3],]);var citations =([['Year', 'Number of Citations'],['2016',26],['2017',0],['2018',21],]);
