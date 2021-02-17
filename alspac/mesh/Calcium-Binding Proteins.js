a3cc0884a289df59cfaaaf882a4d0739cab4134d
Azure pipeline <you@example.com>
2021-02-17 01:48:28 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Calcium-Binding Proteins.js
+++ alspac/mesh/Calcium-Binding Proteins.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',1],['2015',2],]);var citations =([['Year', 'Number of Citations'],['2014',284],['2015',92],]);
+var papers =([['Year', 'Number of papers'],['2014',1],['2015',2],]);var citations =([['Year', 'Number of Citations'],['2014',285],['2015',92],]);
