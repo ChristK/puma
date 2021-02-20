75dec02a57a597374a85854d14db1dcc21265f02
Azure pipeline <you@example.com>
2021-02-20 01:47:34 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Tensile Strength.js
+++ alspac/mesh/Tensile Strength.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2006',1],['2007',0],['2008',1],]);var citations =([['Year', 'Number of Citations'],['2006',269],['2007',0],['2008',68],]);
+var papers =([['Year', 'Number of papers'],['2006',1],['2007',0],['2008',1],]);var citations =([['Year', 'Number of Citations'],['2006',270],['2007',0],['2008',68],]);
