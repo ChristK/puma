b324edc03c8947fc0608eb9386b2a0b6a5b4e3be
Azure pipeline <you@example.com>
2021-02-25 01:52:23 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Diabetes, Gestational.js
+++ alspac/mesh/Diabetes, Gestational.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2010',1],['2011',1],['2012',3],]);var citations =([['Year', 'Number of Citations'],['2010',150],['2011',93],['2012',343],]);
+var papers =([['Year', 'Number of papers'],['2010',1],['2011',1],['2012',3],]);var citations =([['Year', 'Number of Citations'],['2010',150],['2011',93],['2012',344],]);
