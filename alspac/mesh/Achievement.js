972df591e3f217648e7a27f6ee22ad22ee758739
Azure pipeline <you@example.com>
2021-03-02 01:50:11 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Achievement.js
+++ alspac/mesh/Achievement.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',1],['2013',3],['2014',4],['2015',2],]);var citations =([['Year', 'Number of Citations'],['2012',43],['2013',120],['2014',154],['2015',100],]);
+var papers =([['Year', 'Number of papers'],['2012',1],['2013',3],['2014',4],['2015',2],]);var citations =([['Year', 'Number of Citations'],['2012',44],['2013',120],['2014',154],['2015',100],]);
--- alspac/mesh/Transcription, Genetic.js
+++ alspac/mesh/Transcription, Genetic.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2011',1],['2012',1],]);var citations =([['Year', 'Number of Citations'],['2011',276],['2012',479],]);
+var papers =([['Year', 'Number of papers'],['2011',1],['2012',1],]);var citations =([['Year', 'Number of Citations'],['2011',277],['2012',479],]);
