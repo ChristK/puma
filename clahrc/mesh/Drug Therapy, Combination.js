dc2b2cbaa91f5a7f8ed0cc6045dea21a60a4ff96
Azure pipeline <you@example.com>
2021-03-15 02:08:50 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Drug Therapy, Combination.js
+++ clahrc/mesh/Drug Therapy, Combination.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',0],['2018',2],['2019',1],]);var citations =([['Year', 'Number of Citations'],['2016',14],['2017',0],['2018',7],['2019',1],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',0],['2018',2],['2019',1],]);var citations =([['Year', 'Number of Citations'],['2016',14],['2017',0],['2018',8],['2019',1],]);
