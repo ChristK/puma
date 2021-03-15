0cbadba2da2473d8bcee5c69598cd1adc3ca0fc8
Azure pipeline <you@example.com>
2021-03-15 01:47:10 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Fatty Acids, Unsaturated.js
+++ alspac/mesh/Fatty Acids, Unsaturated.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2011',1],['2012',1],['2013',1],]);var citations =([['Year', 'Number of Citations'],['2011',129],['2012',42],['2013',8],]);
+var papers =([['Year', 'Number of papers'],['2011',1],['2012',1],['2013',1],]);var citations =([['Year', 'Number of Citations'],['2011',129],['2012',43],['2013',8],]);
--- alspac/mesh/Self-Injurious Behavior.js
+++ alspac/mesh/Self-Injurious Behavior.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',3],['2013',1],['2014',6],['2015',5],]);var citations =([['Year', 'Number of Citations'],['2012',301],['2013',69],['2014',303],['2015',196],]);
+var papers =([['Year', 'Number of papers'],['2012',3],['2013',1],['2014',6],['2015',5],]);var citations =([['Year', 'Number of Citations'],['2012',301],['2013',69],['2014',303],['2015',197],]);
