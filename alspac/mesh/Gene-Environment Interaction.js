4f5bf901d85f590f8742c9f6945631a642a86c54
Azure pipeline <you@example.com>
2021-03-25 01:50:59 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Gene-Environment Interaction.js
+++ alspac/mesh/Gene-Environment Interaction.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',1],['2013',5],['2014',2],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2012',481],['2013',86],['2014',45],['2015',198],]);
+var papers =([['Year', 'Number of papers'],['2012',1],['2013',5],['2014',2],['2015',4],]);var citations =([['Year', 'Number of Citations'],['2012',483],['2013',86],['2014',45],['2015',198],]);
