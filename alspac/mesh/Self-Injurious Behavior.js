e17d2bedd9f352915e04c643df7b9ccf40cb1f7c
Azure pipeline <you@example.com>
2021-03-26 01:51:33 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Self-Injurious Behavior.js
+++ alspac/mesh/Self-Injurious Behavior.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',3],['2013',1],['2014',6],['2015',5],]);var citations =([['Year', 'Number of Citations'],['2012',301],['2013',69],['2014',304],['2015',201],]);
+var papers =([['Year', 'Number of papers'],['2012',3],['2013',1],['2014',6],['2015',5],]);var citations =([['Year', 'Number of Citations'],['2012',301],['2013',69],['2014',304],['2015',202],]);
