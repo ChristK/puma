9dc45e082938348944d6606d5d9572a3460220d5
Azure pipeline <you@example.com>
2021-04-02 01:46:39 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Personality.js
+++ alspac/mesh/Personality.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',2],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2014',86],['2015',329],]);
+var papers =([['Year', 'Number of papers'],['2014',2],['2015',3],]);var citations =([['Year', 'Number of Citations'],['2014',86],['2015',330],]);
