386e95fb8c8bf5de47edc8474254642d1f54c253
Azure pipeline <you@example.com>
2021-04-03 01:51:36 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Genotyping Techniques.js
+++ alspac/mesh/Genotyping Techniques.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',2],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2014',148],['2015',18],]);
+var papers =([['Year', 'Number of papers'],['2014',2],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2014',149],['2015',18],]);
