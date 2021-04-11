7e8f841e2fdb69a4151361cb1ce3e5f7411791ed
Azure pipeline <you@example.com>
2021-04-11 02:06:09 +0000

    Automatically updating web files for clahrc

--- clahrc/mesh/Emergency Service, Hospital.js
+++ clahrc/mesh/Emergency Service, Hospital.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',2],['2018',1],['2019',1],]);var citations =([['Year', 'Number of Citations'],['2016',7],['2017',32],['2018',3],['2019',11],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',2],['2018',1],['2019',1],]);var citations =([['Year', 'Number of Citations'],['2016',7],['2017',33],['2018',3],['2019',11],]);
