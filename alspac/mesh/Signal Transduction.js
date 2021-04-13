36c7996457c5442a390fced2612f79e430b2d21c
Azure pipeline <you@example.com>
2021-04-13 01:47:50 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Signal Transduction.js
+++ alspac/mesh/Signal Transduction.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',1],['2013',3],['2014',1],]);var citations =([['Year', 'Number of Citations'],['2012',209],['2013',359],['2014',116],]);
+var papers =([['Year', 'Number of papers'],['2012',1],['2013',3],['2014',1],]);var citations =([['Year', 'Number of Citations'],['2012',209],['2013',360],['2014',116],]);
