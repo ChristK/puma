77686fd3298b87e6c615bd1a651372585b5a84d5
Azure pipeline <you@example.com>
2021-04-14 01:48:05 +0000

    Automatically updating web files for alspac

--- alspac/mesh/Confidentiality.js
+++ alspac/mesh/Confidentiality.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',1],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2014',78],['2015',36],]);
+var papers =([['Year', 'Number of papers'],['2014',1],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2014',79],['2015',36],]);
